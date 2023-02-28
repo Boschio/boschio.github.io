1222123ba4fbdd31ab66b44abdf4c831cd0f19d3
Boschio <boschio91@gmail.com>
2023-02-28 09:39:34 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
         <p>
           My name is James A. Bosch and I'm going to master React!
         </p>
+        <a
+          className="App-link"
+          href="https://github.com/Boschio/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check Me Out!
+        </a>
       </header>
     </div>
   );
